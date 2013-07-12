6e076c87782269051bda95816b713cf8491658e1
Bill Brown <billybrown00@gmail.com>
2013-07-12 17:01:06 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+
+// when the page loads
+$(document).ready(function() {
+
+});
+
+// after the page loads
+$(window).load(function() {
+
+});
+
+
+// when the window resizes and only once after it resizes
+$(window).resize(function () {
+
+});
