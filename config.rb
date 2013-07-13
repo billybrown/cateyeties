39da456a61ea0e1d901a98783bff12d634aabeb9
Bill Brown <billybrown00@gmail.com>
2013-07-12 19:49:13 -0500

    made sure cat eye ties was working correctly

--- config.rb
+++ config.rb
@@ -1,12 +1,8 @@
 # Require any additional compass plugins here.
 
-require 'compass'
-require 'color-schemer'
-require 'breakpoint'
-require 'singularitygs'
-
 # Set this to the root of your project when deployed:
 http_path = "/"
+#css_dir = "_site/css"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
--- js/main.js
+++ js/main.js
@@ -1,18 +1,60 @@
 
 
+  // This is a functions that scrolls to #{blah}link
+function goToByScroll(id){
+      // Remove "link" from the ID
+    id = id.replace("link", "");
+      // Scroll
+    $('html,body').animate({
+        scrollTop: $("#"+id).offset().top}, 'slow');
+}
 
-// when the page loads
-$(document).ready(function() {
-
-});
 
 // after the page loads
 $(window).load(function() {
+    var featured_ties_length = 3;
+    var featured_ties_loc = 1;
+
+	$('#arrow-right').click(function(){
+		console.log("length: " + featured_ties_length);
+		console.log("loc: " + featured_ties_loc);
+
+		$('.featured-ties li').removeClass('active');
+		if (featured_ties_loc < featured_ties_length){
+			featured_ties_loc++;
+		} else {
+			featured_ties_loc = 1;
+		}
+		$('.tie-' + featured_ties_loc).addClass('active');
+	});
+
+	$('#arrow-left').click(function(){
+		$('.featured-ties li').removeClass('active');
+		if (featured_ties_loc > 1){
+			featured_ties_loc--;
+		} else {
+			featured_ties_loc = featured_ties_length;
+		}
+		$('.tie-' + featured_ties_loc).addClass('active');
+	});
 
 });
+
 
 
 // when the window resizes and only once after it resizes
 $(window).resize(function () {
+});
+
+// when the page loads
+$(document).ready(function() {
+
+
+		$('.site-header .scroll').click(function(e){
+      	// Call the scroll function
+      	e.preventDefault();
+		goToByScroll($(this).attr("data"));         
+	});
+
 
 });
